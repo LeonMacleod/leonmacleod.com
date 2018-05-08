f217a7857e1554e50a712c99c6921ba713400478
Leon Macleod <leondeanmacleod@gmail.com>
2018-05-08 17:25:32 +1200

    Javascript automatic typing
    In order to mimic a command prompt, decided to use the library `typed.js` instead of spending ages trying to manually code it.

---
+++ js.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+    var typed = new Typed('#typed', {
+      strings: ["<p>`C:\\people\\leonmacleod>`^500pushd d:\\leonmacleod.com\\mylife ^1000\n <br/>`d:\\leonmacleod.com\\mylife>` ^1000 dir <br/> ^1000\n <br/>`Volume in drive D has no label.` <br/><br/> `Directory of d:\\leonmacleod.com\\mylife` <br/><br/> `02/10/2001 12:00AM DIR <a href=\"about.html\">about<a/>`<br/> `02/10/2001 12:00AM DIR <a href=\"papers.html\">papers<a/>` <br/> `02/10/2001 12:00AM DIR <a href=\"projects.html\">projects<a/>` <br/> `02/10/2001 12:00AM DIR <a href=\"research.html\">research<a/>` <p/>"],
+      typeSpeed: 100
+    });
+});
